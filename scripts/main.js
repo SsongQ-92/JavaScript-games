ff8246b7c942debf55fe4b2b1e37931844c863a9
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-08-24 17:49:46 +0900

    feat: 초기 마크업 완료

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1080,
-    height: 800,
+    width: 600,
+    height: 400,
     webPreferences: {
       nodeIntegration: false,
     },
